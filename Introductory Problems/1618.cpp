e43322d0ff84e88ee4ae1946bd265d712ea4ea0b
aziz <mdtareqa90@gmail.com>
2024-03-26 17:12:33 +0600

    Trailing Zeros

---
+++ Introductory Problems/1618.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cstring>
+#include<stack>
+#include<queue>
+#include<cstdio>
+#include<set>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<utility>
+#include<cstdlib>
+using namespace std;
+typedef long long int lli;
+typedef size_t idx;
+#define vi vector<int>
+#define pb(n) push_back(n)
+#define ln "\n"
+#define sp ends
+#define newline cout << ln
+#define fastios ios_base::sync_with_stdio(false); cin.tie(0)
+ 
+ 
+int main(){
+	int n;
+	cin >> n;
+	int cnt = 0;
+	for(int i = 5; i <= n; i *= 5){
+		cnt += (n / i);
+	}
+	cout << cnt << ln;
+	return 0;
+}
